477ab1ff05c78b6248c496b0b53d6e25dffe7768
Vishnu Kumar Raut <23a91a4462@aec.edu.in>
2025-01-11 12:00:29 +0530

    Create Profit or Loss.c

---
+++ Profit or Loss.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {  
+    int cost_price, selling_price;
+    scanf("%d", &cost_price);
+    scanf("%d", &selling_price);
+    if (selling_price > cost_price) {
+        printf("Profit\n");
+    } else if (selling_price < cost_price) {
+        printf("Loss\n");
+    } else {
+        printf("No Profit and No Loss\n");
+    }
+    return 0;
+}
